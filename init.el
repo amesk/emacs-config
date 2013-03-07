25978b90f19dba92c03e6d11f3c10c8001986db4
amesk <alexei.eskenazi@transa.com>
2013-03-07 18:41:03 +0400

    elpa-to-submit removed

--- init.el
+++ init.el
@@ -22,8 +22,6 @@
 
 (add-to-list 'load-path dotfiles-dir)
 
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
--- rc/emacs-rc-elpa.el
+++ rc/emacs-rc-elpa.el
@@ -68,7 +68,7 @@ just have to assume it's online."
 (defun regen-autoloads (&optional force-regen)
   "Regenerate the autoload definitions file if necessary and load it."
   (interactive "P")
-  (let ((autoload-dir (concat dotfiles-dir "/elpa-to-submit"))
+  (let ((autoload-dir (concat dotfiles-dir "/plugins"))
         (generated-autoload-file autoload-file))
     (when (or force-regen
               (not (file-exists-p autoload-file))
