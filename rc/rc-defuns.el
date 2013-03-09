0c7ac266c89d90186f6d6a3108ba125057f2e061
amesk <alexei.eskenazi@transa.com>
2013-03-09 14:09:49 +0400

    rubbish removed

--- rc/rc-defuns.el
+++ rc/rc-defuns.el
@@ -62,55 +62,12 @@ Symbols matching the text at point are put first in the completion list."
            (position (cdr (assoc selected-symbol name-and-pos))))
       (goto-char position))))
 
-;;; These belong in coding-hook:
-
-;; We have a number of turn-on-* functions since it's advised that lambda
-;; functions not go in hooks. Repeatedly evaling an add-to-list with a
-;; hook value will repeatedly add it since there's no way to ensure
-;; that a lambda doesn't already exist in the list.
-
-(defun local-column-number-mode ()
-  (make-local-variable 'column-number-mode)
-  (column-number-mode t))
-
-(defun local-comment-auto-fill ()
-  (set (make-local-variable 'comment-auto-fill-only-comments) t)
-  (auto-fill-mode t))
-
-(defun turn-on-hl-line-mode ()
-  (when (> (display-color-cells) 8) (hl-line-mode t)))
-
-(defun turn-on-save-place-mode ()
-  (setq save-place t))
-
-(defun turn-on-whitespace ()
-  (whitespace-mode t))
 
 (defun turn-on-paredit ()
   (paredit-mode t))
 
 (defun turn-off-tool-bar ()
   (tool-bar-mode -1))
-
-(defun turn-on-idle-highlight ()
-  (idle-highlight-mode t))
-
-(defun add-watchwords ()
-  (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
-          1 font-lock-warning-face t))))
-
-(add-hook 'coding-hook 'local-column-number-mode)
-(add-hook 'coding-hook 'local-comment-auto-fill)
-(add-hook 'coding-hook 'turn-on-hl-line-mode)
-(add-hook 'coding-hook 'turn-on-save-place-mode)
-(add-hook 'coding-hook 'pretty-lambdas)
-(add-hook 'coding-hook 'add-watchwords)
-(add-hook 'coding-hook 'turn-on-idle-highlight)
-
-(defun run-coding-hook ()
-  "Enable things that are convenient across all coding buffers."
-  (run-hooks 'coding-hook))
 
 (defun untabify-buffer ()
   (interactive)
@@ -134,15 +91,6 @@ Symbols matching the text at point are put first in the completion list."
     (when file
       (find-file file))))
 
-;; Cosmetic
-
-(defun pretty-lambdas ()
-  (font-lock-add-keywords
-   nil `(("(?\\(lambda\\>\\)"
-          (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                    ,(make-char 'greek-iso8859-7 107))
-                    nil))))))
-
 ;; Other
 
 (defun eval-and-replace ()
@@ -162,36 +110,11 @@ Symbols matching the text at point are put first in the completion list."
   ;; TODO: remove elpa-to-submit once everything's submitted.
   (byte-recompile-directory (concat dotfiles-dir "elpa-to-submit/") 0))
 
-;; (defun regen-autoloads (&optional force-regen)
-;;   "Regenerate the autoload definitions file if necessary and load it."
-;;   (interactive "P")
-;;   (let ((autoload-dir (concat dotfiles-dir "/elpa-to-submit"))
-;;         (generated-autoload-file autoload-file))
-;;     (when (or force-regen
-;;               (not (file-exists-p autoload-file))
-;;               (some (lambda (f) (file-newer-than-file-p f autoload-file))
-;;                     (directory-files autoload-dir t "\\.el$")))
-;;       (message "Updating autoloads...")
-;;       (let (emacs-lisp-mode-hook)
-;;         (update-directory-autoloads autoload-dir))))
-;;   (load autoload-file))
-
 (defun sudo-edit (&optional arg)
   (interactive "p")
   (if (or arg (not buffer-file-name))
       (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
-(defun lorem ()
-  "Insert a lorem ipsum."
-  (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-          "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-          "culpa qui officia deserunt mollit anim id est laborum."))
 
 (defun switch-or-start (function buffer)
   "If the buffer is current, bury it, otherwise invoke the function."
@@ -206,19 +129,11 @@ Symbols matching the text at point are put first in the completion list."
   (interactive)
   (insert (format-time-string "%c" (current-time))))
 
-(defun pairing-bot ()
-  "If you can't pair program with a human, use this instead."
-  (interactive)
-  (message (if (y-or-n-p "Do you have a test for that? ") "Good." "Bad!")))
-
 (defun esk-paredit-nonlisp ()
   "Turn on paredit mode for non-lisps."
   (set (make-local-variable 'paredit-space-delimiter-chars)
        (list ?\"))
   (paredit-mode 1))
-
-(defun esk-space-for-delimiter? (endp delimiter)
-  (not (member major-mode '(ruby-mode espresso-mode js2-mode))))
 
 (eval-after-load 'paredit
   '(add-to-list 'paredit-space-for-delimiter-predicates
@@ -227,10 +142,6 @@ Symbols matching the text at point are put first in the completion list."
 (defun message-point ()
   (interactive)
   (message "%s" (point)))
-
-(defun esk-disapproval ()
-  (interactive)
-  (insert "ಠ_ಠ"))
 
 (defun esk-agent-path ()
   (if (eq system-type 'darwin)
