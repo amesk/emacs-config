13e68a104a7eab54340db60abcacfbb2bf6b1725
amesk <alexei.eskenazi@transa.com>
2013-03-09 19:38:29 +0400

    maximize frame solved

--- rc/rc-maxframe.el
+++ rc/rc-maxframe.el
@@ -1,2 +1,9 @@
 (require 'maxframe)
-(add-hook 'window-setup-hook 'maximize-frame t)
+
+(defun amesk/try-maximize ()
+  (when (display-graphic-p)
+    (if (eq system-type 'gnu/linux)
+        (toggle-fullscreen)
+      (maximize-frame))))
+
+(add-hook 'window-setup-hook 'amesk/try-maximize)
