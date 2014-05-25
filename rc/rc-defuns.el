b8e48380e72535239bfc9f1a572ae62fe48e1fb5
amesk <alexei.eskenazi@transas.com>
2014-05-25 10:58:55 +0400

    Added amesk/tag-word-or-region function (C-x t).

--- rc/rc-defuns.el
+++ rc/rc-defuns.el
@@ -252,6 +252,26 @@ BEG and END (region to sort)."
             (replace-match "" nil nil))
           (goto-char next-line))))))
 
+
+(defun amesk/tag-word-or-region (tag)
+    "Surround current word or region with a given tag."
+    (interactive "sEnter tag (without <>): ")
+    (let (pos1 pos2 bds start-tag end-tag)
+        (setq start-tag (concat "<" tag ">"))
+        (setq end-tag (concat "</" tag ">"))
+        (if (and transient-mark-mode mark-active)
+            (progn
+                (goto-char (region-end))
+                (insert end-tag)
+                (goto-char (region-beginning))
+                (insert start-tag))
+            (progn
+                (setq bds (bounds-of-thing-at-point 'symbol))
+                (goto-char (cdr bds))
+                (insert end-tag)
+                 (goto-char (car bds))
+                 (insert start-tag)))))
+
 (provide 'starter-kit-defuns)
 
 ;;; rc-defuns.el ends here
